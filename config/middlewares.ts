bf592b3935c0cebc56ac27f517a2d5ddfc80916d
Luca <lucastefan.tomescu@gmail.com>
2025-09-19 12:52:25 +0200

    Fix image serving - configure Cloudinary upload provider and security headers

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,32 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'", 
+            'data:', 
+            'blob:', 
+            'https://holleman-cms-production.up.railway.app',
+            'res.cloudinary.com'
+          ],
+          'media-src': [
+            "'self'", 
+            'data:', 
+            'blob:', 
+            'https://holleman-cms-production.up.railway.app',
+            'res.cloudinary.com'
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: env('UPLOAD_PROVIDER', 'local'),
+      providerOptions: env('UPLOAD_PROVIDER') === 'cloudinary' ? {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      } : {
+        sizeLimit: 100000000, // 100MB
+      },
+    },
+  },
+});
