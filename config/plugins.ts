557b5869f1aa3a87e1794d4321dbbe9e3c75fe10
Luca <lucastefan.tomescu@gmail.com>
2025-09-22 16:39:23 +0200

    Simplify Cloudinary config to fix upload errors

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,11 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: '@strapi/provider-upload-cloudinary',
+      provider: 'cloudinary',
       providerOptions: {
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
-        folder: 'holleman-cms', // Force organize in specific folder
       },
     },
   },
