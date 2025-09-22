9675481d7395434432bf31920daf97cad3cb8771
Luca <lucastefan.tomescu@gmail.com>
2025-09-22 19:16:10 +0200

    Simplify development mode config - revert to npm run develop and clean server config

--- config/server.ts
+++ config/server.ts
@@ -4,6 +4,4 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // Force development mode settings
-  url: env('PUBLIC_URL', `http://${env('HOST', '0.0.0.0')}:${env.int('PORT', 1337)}`),
 });
