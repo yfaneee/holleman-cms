cc0f9738c0718eac2831386891688cb3b53a6135
Luca <lucastefan.tomescu@gmail.com>
2025-09-22 16:46:40 +0200

    Temporarily disable Cloudinary to fix 502 server error

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,8 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: env('UPLOAD_PROVIDER', 'local'),
-      providerOptions: env('UPLOAD_PROVIDER') === 'cloudinary' ? {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      } : {
+      provider: 'local',
+      providerOptions: {
         sizeLimit: 100000000, // 100MB
       },
     },
