1ba5565afd3dfdfbb58d4e8eb2142d8430ebd738
Luca <lucastefan.tomescu@gmail.com>
2025-09-22 18:50:48 +0200

    Revert to npm start - fix the crash caused by broken dev script

--- dev-start.js
+++
@@ -1,6 +0,0 @@
-// Force Strapi to run in development mode
-process.env.NODE_ENV = 'development';
-process.env.STRAPI_DEV = 'true';
-
-// Start Strapi
-require('@strapi/strapi/lib/commands/develop');
