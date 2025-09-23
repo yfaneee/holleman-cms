3fd3b610abdc4fb05e64c08ef213cf59b556b3c0
Luca <lucastefan.tomescu@gmail.com>
2025-09-23 14:06:09 +0200

    Add cariere-positions Strapi content type: Enable dynamic job listings management

---
+++ src/api/cariere-positions/controllers/cariere-position.ts
@@ -0,0 +1,7 @@
+/**
+ * cariere-position controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::cariere-position.cariere-position');
---
+++ src/api/cariere-positions/routes/cariere-position.ts
@@ -0,0 +1,7 @@
+/**
+ * cariere-position router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::cariere-position.cariere-position');
